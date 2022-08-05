b24a61ae9e72670dc3df3bc9d6577b0b89da00f9
tengj98 <tengj98@gmail.com>
2022-08-05 16:30:49 -0400

    cleanup

--- main.js
+++ main.js
@@ -1,317 +1,325 @@
-      d3.select("#g-wageschart_svg-box").classed("hidden", true)
-      
-      function hideNote() {
-        // annotation: #path-1, #text-1, #path-2, #text-2
-        d3.select("#path-1").classed('hidden', true)
-        d3.select("#text-1").classed('hidden', true)
-        d3.select("#path-2").classed('hidden', true)
-        d3.select("#text-2").classed('hidden', true)
-      }
-      
-      hideNote()
+var main = d3.select("main");
+var scrolly = main.select("#scrolly");
+var figure = scrolly.select("figure");
+var article = scrolly.select("article");
+var step = article.selectAll(".step");
 
-      d3.select("#step-one").on('stepout', function(e) {
-        if(e.detail.direction === 'up') {
-          // undo the changes from step one
-          hideNote()
-        }
-      })
+// initialize the scrollama
+var scroller = scrollama();
 
-      d3.select("#step-one").on('stepin', function(e) {
-        console.log("Got to step one")
-        // undo changes from step two
-        d3.selectAll("[data-name='africa'] path").style('stroke', 'none')
+// generic window resize listener event
+function handleResize() {
+    // 1. update height of step elements
+    var stepH = Math.floor(window.innerHeight * 0.8);
+    step.style("height", stepH + "px");
 
-        // do changes for step one
-        d3.select("#path-1").classed('hidden', false)
-        d3.select("#text-1").classed('hidden', false)
-        d3.select("#path-2").classed('hidden', false)
-        d3.select("#text-2").classed('hidden', false)
-      })
+    var figureHeight = window.innerHeight * 0.8;
+    var figureMarginTop = (window.innerHeight - figureHeight)/ 2 ;
 
-      d3.select("#step-two").on('stepin', function(e) {
+    figure
+        .style("height", figureHeight + "px")
+        .style("top", figureMarginTop + "px");
 
-        // undo changes from step one
-        hideNote()
+    // 3. tell scrollama to update new element dimensions
+    scroller.resize();
+}
+    function show(selector) {
+        d3.selectAll(selector)
+            .transition()
+            .duration(200)
+            .attr("opacity", 1)
+    }
+    // this function hides the element passed to it by setting opacity to 0
+    function hide(selector) {
+        d3.selectAll(selector)
+            .transition()
+            .duration(200)
+            .attr("opacity", 0)
+    }
 
-        // do changes for step two
-        console.log("Got to step two")
-        d3.selectAll("[data-name='africa'] path").style('stroke', 'black')
-      })
+// scrollama event handlers
+function handleStepEnter(response) {
+    console.log(response);
+    // response = { element, direction, index }
 
-      d3.select("#step-three").on('stepin', function(e) {
-        // undo changes from step two
-        d3.selectAll("[data-name='africa'] path").style('stroke', 'none')
-      })
+    // add color to current step only
+    step.classed("is-active", function (d, i) {
+        return i === response.index;
+    });
+
+    // update graphic based on step
+    if (response.index === 0) {
+        drawbotChart();
+        d3.select("#botninetyaxis").classed('hidden', false)    
+        d3.select("#botninetyline").classed('hidden', false)   
+        d3.selectAll("#topfiveaxis").classed('hidden', true)    
+        d3.selectAll("#topfiveline").classed('hidden', true)   
+        d3.selectAll("#toponeaxis").classed('hidden', true)    
+        d3.selectAll("#toponeline").classed('hidden', true)  
+        d3.selectAll("#botninetyline5").classed('hidden', true)    
+        d3.selectAll("#botninetyline1").classed('hidden', true)    
+        d3.selectAll("#topfiveline1").classed('hidden', true)  
+ 
+    }
+
+    if (response.index === 1) {
+        drawfiveChart();
+        d3.selectAll("#botninetyaxis").classed('hidden', true)    
+        d3.selectAll("#botninetyline").classed('hidden', true)   
+        d3.selectAll("#topfiveaxis").classed('hidden', false)  
+        d3.select("#botninetyline5").classed('hidden', false)    
+        d3.select("#topfiveline").classed('hidden', false)   
+        d3.selectAll("#toponeaxis").classed('hidden', true)    
+        d3.selectAll("#toponeline").classed('hidden', true)
+        d3.selectAll("#botninetyline1").classed('hidden', true)    
+        d3.selectAll("#topfiveline1").classed('hidden', true)  
+
+    }
+    
+    if (response.index === 2) {
+        drawoneChart();
+        d3.selectAll("#botninetyaxis").classed('hidden', true)    
+        d3.selectAll("#botninetyline").classed('hidden', true)   
+        d3.selectAll("#topfiveaxis").classed('hidden', true)  
+        d3.selectAll("#botninetyline5").classed('hidden', true)    
+        d3.selectAll("#topfiveline").classed('hidden', true)   
+        d3.selectAll("#topfiveaxis").classed('hidden', true)    
+        d3.select("#botninetyline1").classed('hidden', false)    
+        d3.select("#topfiveline1").classed('hidden', false)  
+        d3.selectAll("#toponeaxis").classed('hidden', false)    
+        d3.selectAll("#toponeline").classed('hidden', false)   
+    }
+
+    // figure.select("p").text(response.index + 1);
+
+}
+
+// format dollar signs on axes
+var dollarFormat = function(d) { return '$' + d3.format(',.0f')(d) };
 
 
-// start of somethign newwwwwww
+// draw axes and the line for bottom ninety percent
+function drawbotChart() {
+    const svg = d3.select("svg")
+    
+    svg
+        .attr("viewBox", "0 0 960 960")
+
+    var figureMarginTop = (window.innerHeight - (window.innerHeight*0.8))/ 2 ;
+
+    var max = d3.max(data, d => +d.botninety)
+
+    const titleText = svg 
+        .append("text")
+        .attr("class", "titletext")
+        .attr("transform", "translate(480,30)")
+        .attr("text-anchor", "middle")
+        .text("Pay for the top 0.1%, 5%, and bottom 90%, 1947-2020")
+
+    titleText
+
+    const dateScale = d3.scaleTime()
+        .domain([1947, 2020])
+        .range([60, 900])
+
+    const yScale = d3.scaleLinear()
+        .domain([0, max])
+        .range([450, 50])
+
+    const ninetylineGenerator = d3
+        .line()
+        .x((d,i) => { return dateScale(d.year) })
+        .y((d,i) => { return yScale(d.botninety) })
+        
+    const dateAxis = d3.axisBottom(dateScale)
+        .tickFormat((d,i) => { return d })
+        .tickFormat(d3.format('.0f'))
+        .tickSizeOuter(0)
+
+    const wageAxis = d3.axisLeft(yScale)
+        .tickFormat(dollarFormat)
+        // .tickValues([425000, 850000, 1275000, 1700000, 2125000, 2550000, 2975000, 3400000])
+        // .tickSize(5)
+        // .tickSizeOuter(0)
+        // .tickPadding(5)
+
+    svg 
+        .append("g")
+        .attr("transform", "translate(0,450)")
+        .call(dateAxis)
+
+    svg
+        .append("path")
+        .datum(data)
+        .attr("class", "line")
+        .attr("id", "botninetyline")
+        .attr("d", ninetylineGenerator)
+        .style("opacity", 0) // fade in
+        .transition()
+        .duration(600)
+        .style("opacity", 1)
+
+    svg 
+        .append("g")
+        .attr("transform", "translate(60,0)")
+        .attr("id","botninetyaxis")
+        .call(wageAxis)
+
+    
+}
+
+// draw the line for top five percent
+
+function drawfiveChart() {
+    const svg = d3.select("svg")
+    svg.attr("viewBox", "0 0 960 960")
+    
+    var max = d3.max(data, d => +d.topfive)
+
+    const dateScale = d3.scaleTime()
+        .domain([1947, 2020])
+        .range([60, 900])
+
+    const yScale = d3.scaleLinear()
+        .domain([0, max])
+        .range([450, 50])
+
+    const wageAxis = d3.axisLeft(yScale)
+        .tickFormat(dollarFormat)
 
 
-      var main = d3.select("main");
-      var scrolly = main.select("#scrolly");
-      var figure = scrolly.select("figure");
-      var article = scrolly.select("article");
-      var step = article.selectAll(".step");
-      
-      // initialize the scrollama
-      var scroller = scrollama();
-      
-      // generic window resize listener event
-      function handleResize() {
-          // 1. update height of step elements
-          var stepH = Math.floor(window.innerHeight * 0.75);
-          step.style("height", stepH + "px");
-      
-          var figureHeight = window.innerHeight * 0.8;
-          var figureMarginTop = (window.innerHeight - figureHeight)/ 2 ;
-      
-          figure
-              .style("height", figureHeight + "px")
-              .style("top", figureMarginTop + "px");
-      
-          // 3. tell scrollama to update new element dimensions
-          scroller.resize();
-      }
-      
-      // scrollama event handlers
-      function handleStepEnter(response) {
-          console.log(response);
-          // response = { element, direction, index }
-      
-          // add color to current step only
-          step.classed("is-active", function (d, i) {
-              return i === response.index;
-          });
-      
-          // update graphic based on step
-          if (response.index === 0) {
-              drawbotChart();
-          }
-      
-          if (response.index === 1) {
-              drawfiveChart();
-          }
-          
-          if (response.index === 2) {
-              drawoneChart();
-          }
-      
-          // figure.select("p").text(response.index + 1);
-      
-      }
-      
-      // draw axes and the line for bottom ninety percent
-      function drawbotChart() {
-          const svg = d3.select("svg")
-          
-          svg
-              .attr("viewBox", "0 0 960 960")
-      
-          var figureMarginTop = (window.innerHeight - (window.innerHeight*0.8))/ 2 ;
-      
-          var max = d3.max(data, d => +d.botninety)
-      
-          const dateScale = d3.scaleTime()
-              .domain([1947, 2020])
-              .range([60, 900])
-      
-          const yScale = d3.scaleLinear()
-              .domain([0, max])
-              .range([450, 50])
-      
-          const ninetylineGenerator = d3
-              .line()
-              .x((d,i) => { return dateScale(d.year) })
-              .y((d,i) => { return yScale(d.botninety) })
-              
-          const dateAxis = d3.axisBottom(dateScale)
-              .tickFormat((d,i) => { return d.year})
-              .tickSizeOuter(0)
-      
-          const wageAxis = d3.axisLeft(yScale)
-              // .tickValues([425000, 850000, 1275000, 1700000, 2125000, 2550000, 2975000, 3400000])
-              // .tickSize(5)
-              // .tickSizeOuter(0)
-              // .tickPadding(5)
-      
-          svg 
-              .append("g")
-              .attr("transform", "translate(0,450)")
-              .call(dateAxis)
-              .style("top", figureMarginTop + "px");
-      
-          svg 
-              .append("g")
-              .attr("transform", "translate(60,0)")
-              .call(wageAxis)
-              .style("top", figureMarginTop + "px");
-      
-          svg
-              .append("path")
-              .datum(data)
-              .attr("class", "line")
-              .attr("id", "botninetyline")
-              .attr("d", ninetylineGenerator)
-              .style("opacity",0) // fade in
-              .transition()
-              .duration(600)
-              .style("opacity", 1)
-      
-          
-      }
-      
-      // draw the line for top five percent
-      
-      function drawfiveChart() {
-          const svg = d3.select("svg")
-          svg.attr("viewBox", "0 0 960 960")
-          
-          var max = d3.max(data, d => +d.topfive)
-      
-          const dateScale = d3.scaleTime()
-              .domain([1947, 2020])
-              .range([60, 900])
-      
-          const yScale = d3.scaleLinear()
-              .domain([0, max])
-              .range([450, 50])
-      
-          const wageAxis = d3.axisLeft(yScale)
-      
-          const fivelineGenerator = d3
-              .line()
-              .x((d,i) => { return dateScale(d.year) })
-              .y((d,i) => { return yScale(d.topfive) })
-      
-          const ninetylineGenerator = d3
-              .line()
-              .x((d,i) => { return dateScale(d.year) })
-              .y((d,i) => { return yScale(d.botninety) })
-          
-          svg 
-              .append("g")
-              .attr("transform", "translate(60,0)")
-              .call(wageAxis)
-              // .style("top", figureMarginTop + "px");
-      
-          svg
-              .append("path")
-              .datum(data)
-              .attr("class", "line")
-              .attr("id", "botninetyline")
-              .attr("d", ninetylineGenerator)
-              .style("opacity",0) // fade in
-              .transition()
-              .duration(600)
-              .style("opacity", 1)
-      
-          svg
-              .append("path")
-              .datum(data)
-              .attr("class", "line")
-              .attr("id", "topfiveline")
-              .attr("d", fivelineGenerator)
-              .attr("transform", "translate(0,50)")
-              .style("opacity",0) // fade in
-              .transition()
-              .duration(600)
-              .style("opacity", 1)
-          
-      }
-      
-      function drawoneChart() {
-          const svg = d3.select("svg")
-          svg.attr("viewBox", "0 0 960 960")
-      
-          var max = d3.max(data, d => +d.topone)
-      
-          const dateScale = d3.scaleTime()
-              .domain([1947, 2020])
-              .range([60, 900])
-      
-          const yScale = d3.scaleLinear()
-              .domain([0, max])
-              .range([450, 50])
-      
-          const wageAxis = d3.axisLeft(yScale)
-      
-          const ninetylineGenerator = d3
-              .line()
-              .x((d,i) => { return dateScale(d.year) })
-              .y((d,i) => { return yScale(d.botninety) })
-      
-          const fivelineGenerator = d3
-              .line()
-              .x((d,i) => { return dateScale(d.year) })
-              .y((d,i) => { return yScale(d.topfive) })
-      
-          const onelineGenerator = d3
-              .line()
-              .x((d,i) => { return dateScale(d.year) })
-              .y((d,i) => { return yScale(d.topone) })
-          
-          svg 
-              .append("g")
-              .attr("transform", "translate(60,0)")
-              .call(wageAxis)
-              // .style("top", figureMarginTop + "px");
-      
-          svg
-              .append("path")
-              .datum(data)
-              .attr("class", "line")
-              .attr("id", "botninetyline")
-              .attr("d", ninetylineGenerator)
-              .style("opacity",0) // fade in
-              .transition()
-              .duration(600)
-              .style("opacity", 1)
-      
-          svg
-              .append("path")
-              .datum(data)
-              .attr("class", "line")
-              .attr("id", "topfiveline")
-              .attr("d", fivelineGenerator)
-              .attr("transform", "translate(0,50)")
-              .style("opacity",0) // fade in
-              .transition()
-              .duration(600)
-              .style("opacity", 1)
-          
-          svg
-              .append("path")
-              .datum(data)
-              .attr("class", "line")
-              .attr("id", "topfiveline")
-              .attr("d", onelineGenerator)
-              .attr("transform", "translate(0,50)")
-              .style("opacity",0) // fade in
-              .transition()
-              .duration(600)
-              .style("opacity", 1)
-          
-      }
-      
-      
-      function init() {
-      
-          // 1. force a resize on load to ensure proper dimensions are sent to scrollama
-          handleResize();
-      
-          // 2. setup the scroller passing options
-          // 		this will also initialize trigger observations
-          // 3. bind scrollama event handlers (this can be chained like below)
-          scroller
-              .setup({
-                  step: "#scrolly article .step",
-                  offset: 0.33,
-                  debug: false
-              })
-              .onStepEnter(handleStepEnter);
-      }
-      
-      // kick things off
-      init();
-      
+    const fivelineGenerator = d3
+        .line()
+        .x((d,i) => { return dateScale(d.year) })
+        .y((d,i) => { return yScale(d.topfive) })
+
+    const ninetylineGenerator = d3
+        .line()
+        .x((d,i) => { return dateScale(d.year) })
+        .y((d,i) => { return yScale(d.botninety) })
+
+
+    svg
+        .append("path")
+        .datum(data)
+        .attr("class", "line")
+        .attr("id", "botninetyline5")
+        .attr("d", ninetylineGenerator)
+        .style("opacity",0) // fade in
+        .transition()
+        .duration(600)
+        .style("opacity", 1)
+
+    svg
+        .append("path")
+        .datum(data)
+        .attr("class", "line")
+        .attr("id", "topfiveline")
+        .attr("d", fivelineGenerator)
+        .style("opacity",0) // fade in
+        .transition()
+        .duration(600)
+        .style("opacity", 1)
+
+    svg 
+        .append("g")
+        .attr("transform", "translate(60,0)")
+        .attr("id","topfiveaxis")
+        .call(wageAxis)
+    
+}
+
+function drawoneChart() {
+    const svg = d3.select("svg")
+    svg.attr("viewBox", "0 0 960 960")
+
+    var max = d3.max(data, d => +d.topone)
+
+    const dateScale = d3.scaleTime()
+        .domain([1947, 2020])
+        .range([60, 900])
+
+    const yScale = d3.scaleLinear()
+        .domain([0, max])
+        .range([450, 50])
+
+    const wageAxis = d3.axisLeft(yScale)
+        .tickFormat(dollarFormat)
+
+    const ninetylineGenerator = d3
+        .line()
+        .x((d,i) => { return dateScale(d.year) })
+        .y((d,i) => { return yScale(d.botninety) })
+
+    const fivelineGenerator = d3
+        .line()
+        .x((d,i) => { return dateScale(d.year) })
+        .y((d,i) => { return yScale(d.topfive) })
+
+    const onelineGenerator = d3
+        .line()
+        .x((d,i) => { return dateScale(d.year) })
+        .y((d,i) => { return yScale(d.topone) })
+
+
+    svg
+        .append("path")
+        .datum(data)
+        .attr("class", "line")
+        .attr("id", "botninetyline1")
+        .attr("d", ninetylineGenerator)
+        .style("opacity",0) // fade in
+        .transition()
+        .duration(600)
+        .style("opacity", 1)
+
+    svg
+        .append("path")
+        .datum(data)
+        .attr("class", "line")
+        .attr("id", "topfiveline1")
+        .attr("d", fivelineGenerator)
+        .style("opacity",0) // fade in
+        .transition()
+        .duration(600)
+        .style("opacity", 1)
+    
+    svg
+        .append("path")
+        .datum(data)
+        .attr("class", "line")
+        .attr("id", "toponeline")
+        .attr("d", onelineGenerator)
+        .style("opacity",0) // fade in
+        .transition()
+        .duration(600)
+        .style("opacity", 1)
+
+    svg 
+        .append("g")
+        .attr("transform", "translate(60,0)")
+        .attr("id","toponeaxis")
+        .call(wageAxis)
+    
+}
+
+
+function init() {
+
+    // 1. force a resize on load to ensure proper dimensions are sent to scrollama
+    handleResize();
+
+    // 2. setup the scroller passing options
+    // 		this will also initialize trigger observations
+    // 3. bind scrollama event handlers (this can be chained like below)
+    scroller
+        .setup({
+            step: "#scrolly article .step",
+            offset: 0.33,
+            debug: false
+        })
+        .onStepEnter(handleStepEnter);
+}
+
+// kick things off
+init();
